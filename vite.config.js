bcc8d3da2aefcc9a553d7a0b5d62ecd162eb55b3
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-09-22 18:38:27 -0300

    Bugfixes

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
         short_name: 'BOCALC',
         description: 'The calculator of the biggest football club',
         theme_color: '#ffffff',
+        start_url: '%PUBLIC_URL%/react-calculator/',
         icons: [
           {
             src: 'pwa-192x192.png',
