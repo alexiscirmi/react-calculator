2d0224e492e7fa943a80e51586768d9fea7cb1d9
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-09-25 20:25:40 -0300

    Keyboard arrows can navigate through display characters

--- src/components/context/CalcContext.jsx
+++ src/components/context/CalcContext.jsx
@@ -11,7 +11,7 @@ export const CalcContextComponent = ({ children }) => {
     setDisplay(e)
   }
 
-  const allowedKeys = ['Backspace', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(', ')', '+', '-', '*', '/', '%', '=', '.', 'Enter']
+  const allowedKeys = ['Backspace', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(', ')', '+', '-', '*', '/', '%', '=', '.', 'Enter', 'ArrowLeft', 'ArrowRight']
 
   const [focusDisplay, setFocusDisplay] = useState(false)
 
