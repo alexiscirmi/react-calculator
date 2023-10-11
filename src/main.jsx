7bc9c37c3ef1a9132376a719eea3bd42ed4f48aa
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-10-11 02:08:43 -0300

    index.css => main.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { CalcProvider } from './context/calcContext'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
+import './main.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
