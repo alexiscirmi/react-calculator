bdc98fc9a21955956f989da830adeda01c0f3591
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-10-30 12:37:26 -0300

    Project size optimized

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,35 +7,5 @@ export default defineConfig({
   server: { host: true },
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'prompt',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'BoCALC',
-        short_name: 'BoCALC',
-        description: 'The calculator of the biggest football club',
-        theme_color: '#ffffff',
-        start_url: './',
-        scope: '.',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
   ],
 })
